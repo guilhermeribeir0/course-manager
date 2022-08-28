1f076b8524d5fa9eb7b0a8856eead7a845cd89e3
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-08-28 20:33:23 -0300

    adicionado angular material

--- frontend/course-manager/src/app/app.module.ts
+++ frontend/course-manager/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course/components/course-list/course-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
     
   ],
   providers: [],
