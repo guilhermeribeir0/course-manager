3f390ca3d18107731b635a5726d85d8cc528e18b
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-08-28 20:26:15 -0300

    criado componente nav-bar

--- frontend/course-manager/src/app/app.module.ts
+++ frontend/course-manager/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course/components/course-list/course-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
