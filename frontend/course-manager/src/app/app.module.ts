5a38522db6f4095eda56e0f9a7d0a110164b8e6c
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-08-28 20:19:49 -0300

    criado componente de lista

--- frontend/course-manager/src/app/app.module.ts
+++ frontend/course-manager/src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course/components/course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
