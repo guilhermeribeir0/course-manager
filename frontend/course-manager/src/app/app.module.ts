d238338ca59a99119a97c2fbe9482002f394fdc0
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-08-28 21:08:14 -0300

    criado componente header

--- frontend/course-manager/src/app/app.module.ts
+++ frontend/course-manager/src/app/app.module.ts
@@ -10,13 +10,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
