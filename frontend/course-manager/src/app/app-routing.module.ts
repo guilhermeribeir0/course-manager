8b136f4f65b43583e7c8ebb8295430375c4652ed
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-09-13 21:46:53 -0300

    rota para informacoes

--- frontend/course-manager/src/app/app-routing.module.ts
+++ frontend/course-manager/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [{
   path: 'courses',
   component: CourseListComponent
 }, {
-  path: 'courses/info/:id',
+  path: 'info/:id',
   component: CourseInfoComponent
 }];
 
--- frontend/course-manager/src/app/course/components/course-info/course-info.component.ts
+++ frontend/course-manager/src/app/course/components/course-info/course-info.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CourseService } from './../../course.service';
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../../course';
 
 @Component({
   selector: 'app-course-info',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseInfoComponent implements OnInit {
 
-  constructor() { }
+  course!: Course;
+
+  constructor(private courseService: CourseService, private router: RouterModule, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+      this.courseService.retrieveById(+!this.route.snapshot.paramMap.get('id')).subscribe({
+        next: course => this.course = course,
+        error: err => console.log('Error', err)
+      })
   }
 
 }
