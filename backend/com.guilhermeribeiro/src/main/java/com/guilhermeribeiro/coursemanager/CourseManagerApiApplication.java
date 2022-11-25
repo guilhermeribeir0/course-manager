4ba6989ab3d5c4d2699c19ceb2377b791690a651
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2022-11-24 21:22:24 -0300

    modificando base para Java

---
+++ backend/com.guilhermeribeiro/src/main/java/com/guilhermeribeiro/coursemanager/CourseManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeribeiro.coursemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseManagerApiApplication.class, args);
+	}
+
+}
--- frontend/course-manager/src/app/course/course.service.ts
+++ frontend/course-manager/src/app/course/course.service.ts
@@ -8,7 +8,7 @@ import { Observable } from "rxjs";
 })
 export class CourseService {
 
-    private baseUrl: string = 'http://localhost:3001/courses';
+    private baseUrl: string = 'http://localhost:8080/api/courses';
 
     constructor(private http: HttpClient) { }
 
