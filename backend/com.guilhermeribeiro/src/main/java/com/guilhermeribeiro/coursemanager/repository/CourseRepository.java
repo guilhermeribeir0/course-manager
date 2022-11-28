d2fde77ef94d8eb8a179fa7c30b9a07768b598ce
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2022-11-27 22:05:39 -0300

    criando repository

---
+++ backend/com.guilhermeribeiro/src/main/java/com/guilhermeribeiro/coursemanager/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.guilhermeribeiro.coursemanager.repository;
+
+import com.guilhermeribeiro.coursemanager.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository  extends JpaRepository<Course, Long> {
+
+}
