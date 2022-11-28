397e94478354d206a610ab0c2dbf13253fdf81af
guilhermeribeir0 <dev.guilhermeribeiro@gmail.com>
2022-11-27 22:02:04 -0300

    criando entidade curso

---
+++ backend/com.guilhermeribeiro/src/main/java/com/guilhermeribeiro/coursemanager/model/Course.java
@@ -0,0 +1,35 @@
+package com.guilhermeribeiro.coursemanager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "course")
+public class Course implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String imageUrl;
+
+    private double price;
+
+    @Column(length = 8)
+    private String code;
+
+    private int duration;
+
+    private double rating;
+
+    private LocalDate releaseDate;
+
+    private String description;
+
+}
