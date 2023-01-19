02c6f5ea96519693eac584c87ed688b8794b5844
devguilhermeribeiro <dev.guilhermeribeiro@gmail.com>
2023-01-19 11:56:35 -0300

    registration form created

--- frontend/course-manager/src/app/course/components/course-registration/course-registration.component.ts
+++ frontend/course-manager/src/app/course/components/course-registration/course-registration.component.ts
@@ -1,3 +1,4 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Course } from './../../course';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,10 +11,20 @@ export class CourseRegistrationComponent implements OnInit {
 
   course!: Course;
 
+  courseForm = new FormGroup ({
+      code: new FormControl(0, Validators.required),
+      description: new FormControl(),
+      duration: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      price: new FormControl(0, Validators.required),
+      rating: new FormControl(0, Validators.required),
+      releaseDate: new FormControl('', Validators.required)
+  })
+
   constructor() { }
 
   ngOnInit(): void {
-    
+
   }
 
 }
